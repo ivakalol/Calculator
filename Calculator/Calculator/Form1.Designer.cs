2d9399943bbe87b2103deabf9f45cd2df6708537
Ivaylo Chernev <ivko0527@gmail.com>
2025-01-12 21:53:47 +0200

    design

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clear = new System.Windows.Forms.Button();
+            this.times = new System.Windows.Forms.Button();
+            this.devide = new System.Windows.Forms.Button();
+            this.procent = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.clear.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clear.Location = new System.Drawing.Point(25, 371);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 51);
+            this.clear.TabIndex = 1;
+            this.clear.Text = "CE";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // times
+            // 
+            this.times.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.times.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.times.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.times.Location = new System.Drawing.Point(307, 448);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(75, 51);
+            this.times.TabIndex = 2;
+            this.times.Text = "*";
+            this.times.UseVisualStyleBackColor = false;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // devide
+            // 
+            this.devide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.devide.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.devide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.devide.Location = new System.Drawing.Point(307, 371);
+            this.devide.Name = "devide";
+            this.devide.Size = new System.Drawing.Size(75, 51);
+            this.devide.TabIndex = 3;
+            this.devide.Text = "/";
+            this.devide.UseVisualStyleBackColor = false;
+            this.devide.Click += new System.EventHandler(this.devide_Click);
+            // 
+            // procent
+            // 
+            this.procent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.procent.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procent.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.procent.Location = new System.Drawing.Point(213, 371);
+            this.procent.Name = "procent";
+            this.procent.Size = new System.Drawing.Size(75, 51);
+            this.procent.TabIndex = 4;
+            this.procent.Text = "%";
+            this.procent.UseVisualStyleBackColor = false;
+            this.procent.Click += new System.EventHandler(this.procent_Click);
+            // 
+            // plus
+            // 
+            this.plus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.plus.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.plus.Location = new System.Drawing.Point(307, 605);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(75, 125);
+            this.plus.TabIndex = 8;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = false;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // nine
+            // 
+            this.nine.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nine.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nine.Location = new System.Drawing.Point(213, 448);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(75, 51);
+            this.nine.TabIndex = 7;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // eight
+            // 
+            this.eight.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.eight.Location = new System.Drawing.Point(118, 448);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(75, 51);
+            this.eight.TabIndex = 6;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // seven
+            // 
+            this.seven.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.seven.Location = new System.Drawing.Point(25, 448);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(75, 51);
+            this.seven.TabIndex = 5;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // minus
+            // 
+            this.minus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.minus.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.minus.Location = new System.Drawing.Point(307, 525);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 51);
+            this.minus.TabIndex = 12;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = false;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // six
+            // 
+            this.six.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.six.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.six.Location = new System.Drawing.Point(213, 525);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(75, 51);
+            this.six.TabIndex = 11;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // five
+            // 
+            this.five.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.five.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.five.Location = new System.Drawing.Point(118, 525);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(75, 51);
+            this.five.TabIndex = 10;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // four
+            // 
+            this.four.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.four.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.four.Location = new System.Drawing.Point(25, 525);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(75, 51);
+            this.four.TabIndex = 9;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zero.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.zero.Location = new System.Drawing.Point(25, 679);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(75, 51);
+            this.zero.TabIndex = 16;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // three
+            // 
+            this.three.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.three.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.three.Location = new System.Drawing.Point(213, 605);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(75, 51);
+            this.three.TabIndex = 15;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // two
+            // 
+            this.two.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.two.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.two.Location = new System.Drawing.Point(118, 605);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(75, 51);
+            this.two.TabIndex = 14;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // one
+            // 
+            this.one.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.one.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.one.Location = new System.Drawing.Point(25, 605);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(75, 51);
+            this.one.TabIndex = 13;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.point.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.point.Location = new System.Drawing.Point(118, 679);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(75, 51);
+            this.point.TabIndex = 17;
+            this.point.Text = ".";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // equals
+            // 
+            this.equals.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equals.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.equals.Location = new System.Drawing.Point(213, 679);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(75, 51);
+            this.equals.TabIndex = 18;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = true;
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.delete.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.delete.Location = new System.Drawing.Point(118, 371);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 51);
+            this.delete.TabIndex = 19;
+            this.delete.Text = "⌫";
+            this.delete.UseVisualStyleBackColor = false;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(407, 756);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.procent);
+            this.Controls.Add(this.devide);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.clear);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button devide;
+        private System.Windows.Forms.Button procent;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+
+        public Calculator()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void previewPanel_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void one_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void times_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void devide_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void procent_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
